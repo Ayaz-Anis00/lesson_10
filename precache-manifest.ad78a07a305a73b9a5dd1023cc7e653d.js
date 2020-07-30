0f0938ba96f844dbb8c492b38aa815fb16005cbc
Ayaz-Anis00 <58621127+Ayaz-Anis00@users.noreply.github.com>
2020-07-30 15:34:42 +0530

    Updates

---
+++ precache-manifest.ad78a07a305a73b9a5dd1023cc7e653d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d47d58215b20846e8b5e6536cc383e5",
+    "url": "/lesson_10/index.html"
+  },
+  {
+    "revision": "0e5a1c8d734b743a99a5",
+    "url": "/lesson_10/static/css/main.f16e6c80.chunk.css"
+  },
+  {
+    "revision": "765b166d7ed785af1e19",
+    "url": "/lesson_10/static/js/2.37880c3b.chunk.js"
+  },
+  {
+    "revision": "0e5a1c8d734b743a99a5",
+    "url": "/lesson_10/static/js/main.50ef4d5d.chunk.js"
+  },
+  {
+    "revision": "46189f7ac238a4e020b3",
+    "url": "/lesson_10/static/js/runtime~main.393c9349.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/lesson_10/static/media/crown.19ef7b17.svg"
+  }
+]);
